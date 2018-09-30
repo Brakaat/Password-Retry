d06b2188046751705ad60124bb393f9811963cd0
Moszi <niecheeel@gmail.com>
2018-09-30 17:30:52 +0800

    change way to countdown

--- pwd.py
+++ pwd.py
@@ -1,15 +1,33 @@
+## password count to 3
+# password = 'password123'
+# count = 0
+# while count < 3:
+#     userpwd = input('Enter Password:\n')
+#     count += 1
+#     if userpwd == password:
+#         print('Login Successfully, please wait.')
+#         break
+#     else :
+#         print('Password is not correct!')
+#         print('第', count, '次錯誤')
+#         if count == 3:
+#             print('Sorry,you have tried 3 times!')
+#             break
+
+## password count to 0
 password = 'password123'
-count = 0
-while count < 3:
+count = 3
+while count > 0:
     userpwd = input('Enter Password:\n')
-    count += 1
+    count -= 1
     if userpwd == password:
-        print('Login Successfully, please wait...')
-        print('...')
+        print('Login Successfully, please wait.')
         break
     else :
         print('Password is not correct!')
-        print('第',count,'次錯誤')
-        if count == 3:
+        print('WORNING! You still have', count, 'times to try!')
+        if count == 0:
             print('Sorry,you have tried 3 times!')
             break
+
+
